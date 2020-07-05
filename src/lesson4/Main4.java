93cb768143efa66df13973fe62b5d19adf21e809
koassumo <0770407@gmail.com>
2020-07-05 18:35:50 +0300

    ДЗ 4 (init)

--- src/lesson4/Main4.java
+++ src/lesson4/Main4.java
@@ -1,4 +1,23 @@
 package lesson4;
 
 public class Main4 {
+    public static void main(String[] args) {
+        RelatedList rl = new RelatedList();
+        rl.push(new Cat(2, "cat1"));
+        rl.push(new Cat(3, "cat2"));
+        rl.push(new Cat(4, "cat3"));
+        rl.push(new Cat(5, "cat4"));
+        rl.push(new Cat(6, "cat5"));
+        System.out.println(rl);
+        System.out.println(rl.pop());
+        System.out.println(rl);
+    }
+    //reset();
+    //next();
+    //getCurrent();
+    //hasNext();
+    //atEnd();
+    //insertAfter();
+    //*insertBefore();
+    //deleteCurrent();
 }
